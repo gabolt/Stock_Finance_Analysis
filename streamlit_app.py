aa64973a7ce5c860f3b2845f96f430196d6ffc7a
gabolt <gab.olteanu@gmail.com>
2025-01-23 13:15:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import requests
+import json
+import math
+import pandas as pd
+import numpy as np
+#from scipy.optimize import minimize
+#from scipy.stats import skew, kurtosis, jarque_bera, norm
 
-st.title("🎈 My new app")
+st.title("Finance - Stock Analysis")
+
+
+
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    
 )
