da1dc1a4bedb302c6f2a277ea5ad88c5ae26ac12
gabolt <gab.olteanu@gmail.com>
2025-01-23 13:16:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import math
 import pandas as pd
 import numpy as np
 from scipy.optimize import minimize
-#from scipy.stats import skew, kurtosis, jarque_bera, norm
+from scipy.stats import skew, kurtosis, jarque_bera, norm
 
 st.title("Finance - Stock Analysis")
 
